e1702d22f6c15033853b1df00f6f605a2f594c0c
whale22 < realwhale64@naver.com>
2025-01-06 10:27:41 +0900

    채팅 콘트롤러 제작 / GetMapping을 통한 정상 작동 확인

---
+++ src/main/java/com/ll/chatAI/chatController/ChatController.java
@@ -0,0 +1,30 @@
+package com.ll.chatAI.chatController;
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/api/v1/chat")
+@RestController
+public class ChatController {
+    private final OpenAiChatModel openAiChatModel;
+
+    public ChatController(OpenAiChatModel openAiChatModel) {
+        this.openAiChatModel = openAiChatModel;
+    }
+
+    @GetMapping("/ai")
+    public Map<String, String> chat(@RequestBody String message) {
+        Map<String, String> responses = new HashMap<>();
+
+        String openAiResponse = openAiChatModel.call(message);
+        responses.put("openai(chatGPT) 응답", openAiResponse);
+
+        return responses;
+    }
+}
