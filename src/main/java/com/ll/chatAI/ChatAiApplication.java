616ee37ad8096727e9945d80d50928fa030ef210
whale22 < realwhale64@naver.com>
2025-01-06 10:03:01 +0900

    초기 세팅

---
+++ src/main/java/com/ll/chatAI/ChatAiApplication.java
@@ -0,0 +1,13 @@
+package com.ll.chatAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAiApplication.class, args);
+	}
+
+}
